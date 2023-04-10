f088287d8a18f6139d0d0627459911e151338c7e
ricunidad <ricunidad@gmail.com>
2023-04-10 16:39:00 -0400

    Se separó el mensaje de error en un nuevo componente reutilizable

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Error from "./Error";
 
 const Formulario = ({ pacientes, setPacientes }) => {
 
@@ -45,11 +46,7 @@ const Formulario = ({ pacientes, setPacientes }) => {
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
         onSubmit={handleSubmit}
       >
-        { error && (
-          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
-            <p>Todos los campos son obligatorios</p>
-          </div>
-        ) }
+        { error && <Error mensaje='Todos los campos son obligatorios' /> }
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
           <input 
