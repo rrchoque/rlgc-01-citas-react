fc1ba4722d7cc4b290794124cbba3bac2938016e
ricunidad <ricunidad@gmail.com>
2023-04-14 00:01:24 -0400

    Se agrego la opción de editar registros

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
           pacientes={pacientes}
           setPacientes={setPacientes} 
           paciente = {paciente}
+          setPaciente={setPaciente}
         />
         <ListadoPacientes 
           pacientes={pacientes}
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Error from "./Error";
 
-const Formulario = ({ pacientes, setPacientes, paciente }) => {
+const Formulario = ({ pacientes, setPacientes, paciente, setPaciente }) => {
 
   const [nombre, setNombre] = useState('');
   const [propietario, setPropietario] = useState('');
@@ -11,7 +11,11 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
 
   const [error, setError] = useState(false);
   useEffect(()=> {
-    console.log(paciente)
+    setNombre(paciente.nombre)
+    setPropietario(paciente.propietario)
+    setEmail(paciente.email)
+    setFecha(paciente.fecha)
+    setSintomas(paciente.sintomas)
   }, [paciente])
 
   const generarId = () => {
@@ -29,24 +33,34 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
       console.log('Hay almenos un campo vacio')
       setError(true);
       return;
-    } else {
-      const objetoPaciente = { 
-        nombre, 
-        propietario, 
-        email, 
-        fecha, 
-        sintomas, 
-        id: generarId()
-      };
-      setPacientes([...pacientes, objetoPaciente]);
-
-      // Reiniciar el formulario
-      setNombre('');
-      setPropietario('');
-      setEmail('');
-      setFecha('');
-      setSintomas('');
     }
+
+    const objetoPaciente = { 
+      nombre, 
+      propietario, 
+      email, 
+      fecha, 
+      sintomas
+    };
+
+    if (paciente.id) {
+      // Editar Registro
+      objetoPaciente.id = paciente.id;
+      const pacientesActualizados = pacientes.map(pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState )
+      setPacientes(pacientesActualizados);
+      setPaciente({});
+    } else {
+      // Nuevo Registro
+      objetoPaciente.id = generarId();
+      setPacientes([...pacientes, objetoPaciente]);
+    }
+
+    // Reiniciar el formulario
+    setNombre('');
+    setPropietario('');
+    setEmail('');
+    setFecha('');
+    setSintomas('');
 
     setError(false);
   }
@@ -125,6 +139,7 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
         <input 
           type="submit" 
           className="bg-indigo-600 hover:bg-indigo-700 cursor-pointer transition-colors w-full p-3 text-white font-bold uppercase"
+          value={paciente.id ? 'Editar Paciente':'Agregar Paciente'}
         />
       
       </form>
