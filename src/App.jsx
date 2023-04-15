3987551b3fc1b2dba7907d82c4b363f4713fdfe9
ricunidad <ricunidad@gmail.com>
2023-04-14 23:59:10 -0400

    Se agrego la opción de eliminar pacientes y se habilitó el almacenamiento en localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
@@ -6,8 +6,28 @@ import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
   const [paciente, setPaciente] = useState({});
+
+  // useEffect(() => {
+  //   const obtenerLS = () => {
+  //     const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+  //     setPacientes(pacientesLS);
+  
+  //     console.log(pacientesLS, pacientes)
+  //   }
+
+  //   obtenerLS();
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id != id )
+    setPacientes(pacientesActualizados);
+  }
 
   return (
     <div className="container mx-auto mt-20">
@@ -22,6 +42,7 @@ function App() {
         <ListadoPacientes 
           pacientes={pacientes}
           setPaciente ={setPaciente}
+          eliminarPaciente={eliminarPaciente}
         />
       </div>
     </div>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Paciente = ({paciente, setPaciente}) => {
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
 
-  const {nombre, propietario, email, fecha, sintomas} = paciente;
+  const {nombre, propietario, email, fecha, sintomas, id} = paciente;
+
+  const handleEliminar = () => {
+    const respuesta = confirm('Deseas eliminar este paciente?')
+
+    console.log(respuesta);
+
+    if (respuesta) {
+      eliminarPaciente(id);
+    }
+  }
 
   return (
     <div className='mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
@@ -41,6 +51,7 @@ const Paciente = ({paciente, setPaciente}) => {
         <button
           type='button'
           className='py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg'
+          onClick={ handleEliminar}
         >Eliminar</button>
       </div>
     </div>
